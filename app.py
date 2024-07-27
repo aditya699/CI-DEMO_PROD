f567566f722212237c9242365423b896db9dd985
Aditya Bhatt <meenakshi.bhtt@gmail.com>
2024-07-27 19:41:36 +0530

    changed letter to words

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ def subtract(a, b):
     return a - b
 
 if __name__ == "__main__":
-    a = int(input("Enter Number one: "))
-    b = int(input("Enter Number two: "))
+    a = int(input("Enter Number 1: "))
+    b = int(input("Enter Number 2: "))
     print(subtract(a, b))
